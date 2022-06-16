81a0c88f935d12a739f0b91c10518b9a0b2f8455
Dennis Kikendall <denniskiken@gmail.com>
2022-06-16 08:08:12 -0400

    Initial Commit

---
+++ DynamicToDoList/DynamicToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicToDoListApp.swift
+//  DynamicToDoList
+//
+//  Created by Dennis Kikendall on 6/16/22.
+//
+
+import SwiftUI
+
+@main
+struct DynamicToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
